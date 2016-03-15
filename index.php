4bceba9b008eceff00bb0289fac23b0661fe0ca3
Joseph Cahet <ygorcahet@gmail.com>
2016-03-15 20:59:09 -0300

    tela de login

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<?php include './style/template.php';?>
+<div class="col-sm-5 col-sm-offset-3"  style="margin-top: 250px;">
+    
+    <h4>Discográfica PHP</h4> - <h5>login</h5>
+    <input name="email" class="form-control">
+    <input name="senha" class="form-control">
+    <br>
+    <input type="submit" name="logar" class="btn btn-success" value="Enviar">
+    <input type="reset" class="btn btn-danger" value="limpar">
+</div>
