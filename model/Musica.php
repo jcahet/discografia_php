f7ec55e3a04880f49f81677ae28fc8e21fdbac20
Joseph Cahet <ygorcahet@gmail.com>
2016-03-15 21:37:22 -0300

    musicas

---
+++ model/Musica.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of musica
+ *
+ * @author PHP e MySQL
+ */
+class musica {
+    //put your code here
+}
---
+++ view/index_musicas.php
@@ -0,0 +1,22 @@
+<?php include '../style/template.php';?>
+
+<h2>Lista de Musicas</h2>
+
+<div class="col-sm-10 col-sm-offset-1">
+    <h2>Lista de Musicas</h2>
+    <table class="table table-striped">
+        <tr>
+            <td>id</td>
+            <td>nome</td>
+            <td>autor_id</td>
+        
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Chão de Giz</td>
+            <td>10</td>
+        </tr>
+    </table>
+    
+    
+</div>
